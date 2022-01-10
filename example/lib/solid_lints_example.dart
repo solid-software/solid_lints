5dc6e6784b86fa7141ffad631c692a2b65203453
Illia Romanenko <illia.romanenko@gmail.com>
2022-01-10 15:44:45 +0200

    Remove long-method & source-lines-of-code for tests (#6)
    * fix typo
    
    * Fix no empty line
    
    Co-authored-by: Solid Software <hello@solid.software>

--- example/lib/solid_lints_example.dart
+++ example/lib/solid_lints_example.dart
@@ -1,8 +1,6 @@
 void main() {
-  tooManyParams(1, 2, 3, 4, 5);
+  sum(1, 1);
 }
 
-/// Documentation
-///
-int tooManyParams(int p1, int p2, int p3, int p4, int p5) =>
-    p1 + p2 + p3 + p4 + p5;
+/// Sum of two numbers. Standard dart overflow rules apply.
+int sum(int p1, int p2) => p1 + p2;
---
+++ example/test/solid_lints_example_test.dart
@@ -0,0 +1,368 @@
+import 'package:solid_lints_example/solid_lints_example.dart';
+import 'package:test/test.dart';
+
+class Service {
+  int method() {
+    return 0;
+  }
+}
+
+class ServiceStub implements Service {
+  @override
+  int method() {
+    return 1;
+  }
+}
+
+void main() {
+  //It's handy to use `late` in such situations.
+  late Service service;
+  setUp(() {
+    service = ServiceStub();
+  });
+
+  //intentionally long test method to test for long-method rule
+  const two = 2;
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+  test("addition", () {
+    expect(sum(1, service.method()), equals(two));
+  });
+}
