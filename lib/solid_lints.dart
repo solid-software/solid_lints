948ff64fcdb88dfaa71973460f4aba9d70f82a1c
Nikodem Bernat <n.bernat@outlook.com>
2023-08-09 16:34:00 +0200

    Add support for the number of parameters metric

--- lib/solid_lints.dart
+++ lib/solid_lints.dart
@@ -3,6 +3,8 @@ library solid_metrics;
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:solid_lints/cyclomatic_complexity/cyclomatic_complexity_metric.dart';
 import 'package:solid_lints/cyclomatic_complexity/models/cyclomatic_complexity_parameters.dart';
+import 'package:solid_lints/number_of_parameters/models/number_of_parameters_parameters.dart';
+import 'package:solid_lints/number_of_parameters/number_of_parameters_metric.dart';
 
 /// creates plugin
 PluginBase createPlugin() => _SolidLints();
@@ -38,6 +40,36 @@ class _SolidLints extends PluginBase {
       rules.add(
         CyclomaticComplexityMetric(
           additionalParameters: complexityMetricParameters,
+          code: lintCode,
+        ),
+      );
+    }
+
+    final numberOfParametersRule =
+        configs.rules[NumberOfParametersMetric.lintName];
+
+    final numberOfParametersEnabled = numberOfParametersRule?.enabled ?? false;
+
+    if (numberOfParametersEnabled) {
+      final numberOfParametersParameters =
+          NumberOfParametersParameters.fromJson(
+        _getParamsForLintRule(
+              configs,
+              NumberOfParametersMetric.lintName,
+            ) ??
+            {},
+      );
+
+      final lintCode = LintCode(
+        name: 'number_of_parameters',
+        problemMessage: ''
+            'The maximum allowed number of parameters is '
+            '${numberOfParametersParameters.maxParameters}',
+      );
+
+      rules.add(
+        NumberOfParametersMetric(
+          additionalParameters: numberOfParametersParameters,
           code: lintCode,
         ),
       );
