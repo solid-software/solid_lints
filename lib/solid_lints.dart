9c9c0b39e07ccf02ca576279ada8df14286e1976
Nikodem Bernat <n.bernat@outlook.com>
2023-08-09 17:32:50 +0200

    Simplify configuration and remove redundant code

--- lib/solid_lints.dart
+++ lib/solid_lints.dart
@@ -3,6 +3,7 @@ library solid_metrics;
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:solid_lints/cyclomatic_complexity/cyclomatic_complexity_metric.dart';
 import 'package:solid_lints/cyclomatic_complexity/models/cyclomatic_complexity_parameters.dart';
+import 'package:solid_lints/models/metric_rule.dart';
 import 'package:solid_lints/number_of_parameters/models/number_of_parameters_parameters.dart';
 import 'package:solid_lints/number_of_parameters/number_of_parameters_metric.dart';
 
@@ -15,75 +16,32 @@ class _SolidLints extends PluginBase {
   List<LintRule> getLintRules(CustomLintConfigs configs) {
     final List<LintRule> rules = [];
 
-    final cyclomaticComplexityRule =
-        configs.rules[CyclomaticComplexityMetric.lintName];
-    final cyclomaticComplexityEnabled =
-        cyclomaticComplexityRule?.enabled ?? false;
+    final cyclomaticComplexity = MetricRule<CyclomaticComplexityParameters>(
+      configs: configs,
+      name: CyclomaticComplexityMetric.lintName,
+      factory: CyclomaticComplexityParameters.fromJson,
+      problemMessage: (value) => ''
+          'Please decrease the complexity of function to '
+          'a minimum value ${value.maxComplexity}',
+    );
 
-    if (cyclomaticComplexityEnabled) {
-      final complexityMetricParameters =
-          CyclomaticComplexityParameters.fromJson(
-        _getParamsForLintRule(
-              configs,
-              CyclomaticComplexityMetric.lintName,
-            ) ??
-            {},
-      );
-
-      final lintCode = LintCode(
-        name: 'cyclomatic_complexity_metric',
-        problemMessage: ''
-            'Please decrease the complexity of function to '
-            'a minimum value ${complexityMetricParameters.maxComplexity}',
-      );
-
-      rules.add(
-        CyclomaticComplexityMetric(
-          additionalParameters: complexityMetricParameters,
-          code: lintCode,
-        ),
-      );
+    if (cyclomaticComplexity.enabled) {
+      rules.add(CyclomaticComplexityMetric(cyclomaticComplexity));
     }
 
-    final numberOfParametersRule =
-        configs.rules[NumberOfParametersMetric.lintName];
+    final numberOfParameters = MetricRule<NumberOfParametersParameters>(
+      configs: configs,
+      name: NumberOfParametersMetric.lintName,
+      factory: NumberOfParametersParameters.fromJson,
+      problemMessage: (value) => ''
+          'The maximum allowed number of parameters is ${value.maxParameters}. '
+          'Try reducing the number of parameters.',
+    );
 
-    final numberOfParametersEnabled = numberOfParametersRule?.enabled ?? false;
-
-    if (numberOfParametersEnabled) {
-      final numberOfParametersParameters =
-          NumberOfParametersParameters.fromJson(
-        _getParamsForLintRule(
-              configs,
-              NumberOfParametersMetric.lintName,
-            ) ??
-            {},
-      );
-
-      final lintCode = LintCode(
-        name: 'number_of_parameters',
-        problemMessage: ''
-            'The maximum allowed number of parameters is '
-            '${numberOfParametersParameters.maxParameters}. '
-            'Try reducing the number of parameters.',
-      );
-
-      rules.add(
-        NumberOfParametersMetric(
-          additionalParameters: numberOfParametersParameters,
-          code: lintCode,
-        ),
-      );
+    if (numberOfParameters.enabled) {
+      rules.add(NumberOfParametersMetric(numberOfParameters));
     }
 
     return rules;
-  }
-
-  /// Get params of lint rule
-  Map<String, Object?>? _getParamsForLintRule(
-    CustomLintConfigs configs,
-    String code,
-  ) {
-    return configs.rules[code]?.json;
   }
 }
