9832acef7a328263185dc5f335ed643423553d2f
Nikodem Bernat <n.bernat@outlook.com>
2023-08-10 17:28:35 +0200

    Add `avoid_global_state` lint

--- lib/solid_lints.dart
+++ lib/solid_lints.dart
@@ -1,6 +1,7 @@
 library solid_metrics;
 
 import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:solid_lints/lints/avoid_global_state/avoid_global_state_rule.dart';
 import 'package:solid_lints/lints/avoid_late_keyword/avoid_late_keyword_rule.dart';
 import 'package:solid_lints/lints/avoid_non_null_assertion/avoid_non_null_assertion_rule.dart';
 import 'package:solid_lints/lints/cyclomatic_complexity/cyclomatic_complexity_metric.dart';
@@ -83,6 +84,16 @@ class _SolidLints extends PluginBase {
       rules.add(
         AvoidNonNullAssertionRule(code: avoidNonNullAssertion.lintCode),
       );
+    }
+
+    final avoidGlobalState = MetricRule(
+      configs: configs,
+      name: AvoidGlobalStateRule.lintName,
+      problemMessage: (_) => 'Avoid variables that can be globally mutated.',
+    );
+
+    if (avoidGlobalState.enabled) {
+      rules.add(AvoidGlobalStateRule(code: avoidGlobalState.lintCode));
     }
 
     return rules;
