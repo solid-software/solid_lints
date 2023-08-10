9e545c1e669151beac03adc35923cf5524b9a98a
Nikodem Bernat <n.bernat@outlook.com>
2023-08-10 16:07:45 +0200

    Add test and fix an edge case

---
+++ example/test/lines_of_code_test.dart
@@ -0,0 +1,61 @@
+/// expect_lint: lines_of_code
+/// Check lines of code fail
+///
+/// `lines_of_code: max_length`
+import 'package:test/test.dart';
+
+/// Check number of lines fail
+///
+void linesOfCode() {
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+}
