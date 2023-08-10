147a60c104b3e3e7e2a7d4ef6b5c666e4ff78a04
Nikodem Bernat <n.bernat@outlook.com>
2023-08-10 17:28:35 +0200

    Add `avoid_global_state` lint

---
+++ example/test/avoid_global_state_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: type_annotate_public_apis
+
+/// Check global mutable variable fail
+/// `avoid_global_state`
+
+// expect_lint: avoid_global_state
+var globalMutable = 0;
+
+class Test {
+  // expect_lint: avoid_global_state
+  static int globalMutable = 0;
+}
