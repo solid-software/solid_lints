56f76cb88665343dfe69223896d809fdc28d547d
Nikodem Bernat <n.bernat@outlook.com>
2023-08-10 16:31:22 +0200

    Update lint to match the specification

---
+++ lib/lints/function_lines_of_code/function_lines_of_code_metric.dart
@@ -0,0 +1,40 @@
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:solid_lints/lints/function_lines_of_code/models/function_lines_of_code_parameters.dart';
+import 'package:solid_lints/lints/function_lines_of_code/visitor/function_lines_of_code_visitor.dart';
+import 'package:solid_lints/models/metric_rule.dart';
+
+/// A number of lines metric which checks whether we didn't exceed
+/// the maximum allowed number of lines for a file
+class FunctionLinesOfCodeMetric extends DartLintRule {
+  /// The [LintCode] of this lint rule that represents the error if number of
+  /// parameters reaches the maximum value.
+  static const lintName = 'function_lines_of_code';
+
+  /// Configuration for number of parameters metric rule.
+  final MetricRule<FunctionLinesOfCodeParameters> config;
+
+  /// Creates a new instance of [FunctionLinesOfCodeMetric].
+  FunctionLinesOfCodeMetric(this.config) : super(code: config.lintCode);
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    final visitor = FunctionLinesOfCodeVisitor(resolver.lineInfo);
+
+    context.registry.addDeclaration((node) {
+      node.visitChildren(visitor);
+
+      if (visitor.linesWithCode.length > config.parameters.maxLines) {
+        reporter.reportErrorForOffset(
+          code,
+          node.firstTokenAfterCommentAndMetadata.offset,
+          node.end,
+        );
+      }
+    });
+  }
+}
--- lib/solid_lints.dart
+++ lib/solid_lints.dart
@@ -3,8 +3,8 @@ library solid_metrics;
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:solid_lints/lints/cyclomatic_complexity/cyclomatic_complexity_metric.dart';
 import 'package:solid_lints/lints/cyclomatic_complexity/models/cyclomatic_complexity_parameters.dart';
-import 'package:solid_lints/lints/lines_of_code/lines_of_code_metric.dart';
-import 'package:solid_lints/lints/lines_of_code/models/lines_of_code_parameters.dart';
+import 'package:solid_lints/lints/function_lines_of_code/function_lines_of_code_metric.dart';
+import 'package:solid_lints/lints/function_lines_of_code/models/function_lines_of_code_parameters.dart';
 import 'package:solid_lints/lints/number_of_parameters/models/number_of_parameters_parameters.dart';
 import 'package:solid_lints/lints/number_of_parameters/number_of_parameters_metric.dart';
 import 'package:solid_lints/models/metric_rule.dart';
@@ -44,17 +44,17 @@ class _SolidLints extends PluginBase {
       rules.add(NumberOfParametersMetric(numberOfParameters));
     }
 
-    final linesOfCode = MetricRule<LinesOfCodeParameters>(
+    final functionLinesOfCode = MetricRule<FunctionLinesOfCodeParameters>(
       configs: configs,
-      name: LinesOfCodeMetric.lintName,
-      factory: LinesOfCodeParameters.fromJson,
+      name: FunctionLinesOfCodeMetric.lintName,
+      factory: FunctionLinesOfCodeParameters.fromJson,
       problemMessage: (value) => ''
           'The maximum allowed number of lines is ${value.maxLines}. '
-          'Try splitting the code into multiple files.',
+          'Try splitting this function into smaller parts.',
     );
 
-    if (linesOfCode.enabled) {
-      rules.add(LinesOfCodeMetric(linesOfCode));
+    if (functionLinesOfCode.enabled) {
+      rules.add(FunctionLinesOfCodeMetric(functionLinesOfCode));
     }
 
     return rules;
