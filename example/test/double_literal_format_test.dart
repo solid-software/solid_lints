930934c5c8502ede824fb422aa2482e7e7dcd10b
Vadym Khokhlov <xvadima@ukr.net>
2023-08-30 17:55:59 +0300

    implement the double-literal-format rule

--- example/test/double_literal_format_test.dart
+++ example/test/double_literal_format_test.dart
@@ -1,16 +1,36 @@
 // ignore_for_file: avoid_global_state
+// ignore_for_file: type_annotate_public_apis
+// ignore_for_file:unused_local_variable
 
-/// Check `double-literal-format` rule
+/// Check the `double-literal-format` rule
 
 // expect_lint: double-literal-format
-var a = 05.23;
+var badA = 05.23;
+var goodA = 5.23;
+var stringA = '05.23';
+
+// expect_lint: double-literal-format
+double badB = -01.2;
+double goodB = -1.2;
+
+// expect_lint: double-literal-format
+double badC = -001.2;
+
+// expect_lint: double-literal-format
+double badExpr = 5.23 + 05.23;
+
+double goodExpr = 5.23 + 5.23;
 
 class DoubleLiteralFormatTest {
   // expect_lint: double-literal-format
-  var b = .16e+5;
+  var badA = .16e+5;
+  var goodA = 0.16e+5;
 
   void someMethod() {
     // expect_lint: double-literal-format
-    const c = -0.250;
+    const badA = -0.250;
+    const goodA = -0.25;
+    // expect_lint: double-literal-format
+    const goodB = 0.160e+5;
   }
 }
--- lib/lints/double_literal_format/double_literal_format_rule.dart
+++ lib/lints/double_literal_format/double_literal_format_rule.dart
@@ -12,8 +12,34 @@ import 'package:solid_lints/models/solid_lint_rule.dart';
 /// var a = 5.23, b = 0.16e+5, c = -0.25, d = -0.4e-5;
 class DoubleLiteralFormatRule extends SolidLintRule {
   /// The [LintCode] of this lint rule that represents
-  /// the error whether we use global state.
+  /// the error whether we use bad formatted double literals.
   static const lintName = 'double-literal-format';
+
+  // Use different messages for different issues
+  /// The [LintCode] of this lint rule that represents
+  /// the error whether we use double literals with a redundant leading 0.
+  static const _leadingZeroCode = LintCode(
+    name: lintName,
+    problemMessage: "Double literals shouldn't have redundant leading `0`.",
+    correctionMessage: "Remove redundant leading `0`.",
+  );
+
+  /// The [LintCode] of this lint rule that represents
+  /// the error whether we use double literals with a leading decimal point.
+  static const _leadingDecimalCode = LintCode(
+    name: lintName,
+    problemMessage:
+        "Double literals shouldn't begin with the decimal point `.`.",
+    correctionMessage: "Add missing leading `0`.",
+  );
+
+  /// The [LintCode] of this lint rule that represents
+  /// the error whether we use double literals with a trailing 0.
+  static const _trailingZeroCode = LintCode(
+    name: lintName,
+    problemMessage: "Double literals should not end with a trailing `0`.",
+    correctionMessage: "Remove redundant trailing `0`.",
+  );
 
   DoubleLiteralFormatRule._(super.config);
 
@@ -23,9 +49,7 @@ class DoubleLiteralFormatRule extends SolidLintRule {
     final rule = RuleConfig(
       configs: configs,
       name: lintName,
-      problemMessage: (_) => ''
-          'Double literals should start with `0.` instead of `.`'
-          'Double literals should not end with a trailing `0`',
+      problemMessage: (_) => 'Bad formatted double literal',
     );
 
     return DoubleLiteralFormatRule._(rule);
@@ -37,10 +61,34 @@ class DoubleLiteralFormatRule extends SolidLintRule {
     ErrorReporter reporter,
     CustomLintContext context,
   ) {
-    // TODO: implement run
     context.registry.addDoubleLiteral((node) {
-      print('CHECK double rule $node | ${node.literal}');
-      // reporter.reportErrorForNode(code, node);
+      final lexeme = node.literal.lexeme;
+
+      if (_isLeadingZero(lexeme)) {
+        reporter.reportErrorForNode(_leadingZeroCode, node);
+        return;
+      }
+      if (_isLeadingDecimal(lexeme)) {
+        reporter.reportErrorForNode(_leadingDecimalCode, node);
+        return;
+      }
+      if (_isTrailingZero(lexeme)) {
+        reporter.reportErrorForNode(_trailingZeroCode, node);
+        return;
+      }
     });
+  }
+
+  bool _isLeadingZero(String lexeme) =>
+      lexeme.startsWith('0') && lexeme[1] != '.';
+
+  bool _isLeadingDecimal(String lexeme) => lexeme.startsWith('.');
+
+  bool _isTrailingZero(String lexeme) {
+    final mantissa = lexeme.split('e').first;
+
+    return mantissa.contains('.') &&
+        mantissa.endsWith('0') &&
+        mantissa.split('.').last != '0';
   }
 }
