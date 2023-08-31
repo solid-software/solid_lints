01f39c4e65a786cbe3064ea37056206a4f8e8075
Vadym Khokhlov <xvadima@ukr.net>
2023-08-31 18:33:28 +0300

    add avoid-unnecessary-type-assertions rule ('whereType' part)

--- example/test/avoid_unnecessary_type_assertions_test.dart
+++ example/test/avoid_unnecessary_type_assertions_test.dart
@@ -13,11 +13,20 @@ void fun() {
   // expect_lint: avoid-unnecessary-type-assertions
   testList.whereType<double>();
 
+  final dynamicList = <dynamic>[1.0, 2.0];
+  dynamicList.whereType<double>();
+
+  // just any method, except `whereType`
+  testList.add(4.0);
+
+  // expect_lint: avoid-unnecessary-type-assertions
+  [1.0, 2.0].whereType<double?>();
+
   final double d = 2.0;
   // expect_lint: avoid-unnecessary-type-assertions
   final casted = d is double;
 
   final double? nullableD = 2.0;
-  // `Type? is Type` is allowed
+  // casting `Type? is Type` is allowed
   final castedD = nullableD is double;
 }
