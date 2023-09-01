7c4ae78990f757fb574b9077ab580fbefc8ed833
Vadym Khokhlov <xvadima@ukr.net>
2023-09-01 20:11:47 +0300

    add utility class for type chekcking methods

---
+++ lib/utils/typecast_utils.dart
@@ -0,0 +1,21 @@
+import 'package:analyzer/dart/element/type.dart';
+
+// Useful types for testing expressions' types
+
+// TODO: consider to move here common functions for rules
+// avoid-unnecessary-type-assertions
+// avoid-unnecessary-type-casts (after implementation)
+// avoid-unrelated-type-assertions (after implementation)
+
+/// A class for representing arguments for types checking methods
+class TypeCast {
+  /// The static type ot the tested expression
+  final DartType source;
+
+  /// The type being tested for
+  final DartType target;
+
+  /// Creates a new Typecast object with a given expression's or object's type
+  /// and a tested type
+  TypeCast({required this.source, required this.target});
+}
