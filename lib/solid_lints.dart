d5ea78065de530ae970622a5c028f76fd244b627
Yaroslav <43727448+laptevw@users.noreply.github.com>
2023-09-04 14:15:59 +0300

    implement "avoid unnecessary setstate" rule (#44)
    * implement "avoid unnecessary setstate" rule
    
    * fix pr comments
    
    * added idea files to gitignore
    
    * fix pr comments
    
    * Fix merge conflict
    
    * Fix tests and improve GitHub workflow
    
    * Fix workflow file
    
    * Fix conflicts after merge
    
    * Remove non-existing import
    
    * Add missing rule to tests and remove unnecessary GitHub actions step
    
    ---------
    
    Co-authored-by: Yaroslav Laptiev <yaroslav.laptiev@wmg.com>
    Co-authored-by: vladimir-beloded <x-volodymyr.beloded@transcarent.com>

--- lib/solid_lints.dart
+++ lib/solid_lints.dart
@@ -5,6 +5,7 @@ import 'package:solid_lints/lints/avoid_global_state/avoid_global_state_rule.dar
 import 'package:solid_lints/lints/avoid_late_keyword/avoid_late_keyword_rule.dart';
 import 'package:solid_lints/lints/avoid_non_null_assertion/avoid_non_null_assertion_rule.dart';
 import 'package:solid_lints/lints/avoid_returning_widgets/avoid_returning_widgets_rule.dart';
+import 'package:solid_lints/lints/avoid_unnecessary_setstate/avoid_unnecessary_set_state_rule.dart';
 import 'package:solid_lints/lints/avoid_unnecessary_type_assertions/avoid_unnecessary_type_assertions_rule.dart';
 import 'package:solid_lints/lints/cyclomatic_complexity/cyclomatic_complexity_metric.dart';
 import 'package:solid_lints/lints/double_literal_format/double_literal_format_rule.dart';
@@ -29,6 +30,7 @@ class _SolidLints extends PluginBase {
       AvoidReturningWidgetsRule.createRule(configs),
       DoubleLiteralFormatRule.createRule(configs),
       AvoidUnnecessaryTypeAssertions.createRule(configs),
+      AvoidUnnecessarySetStateRule.createRule(configs),
     ];
 
     // Return only enabled rules
