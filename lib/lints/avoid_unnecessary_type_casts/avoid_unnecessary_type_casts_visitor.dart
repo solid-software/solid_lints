eced630e9399318c94f885b43da58f9f4b4ea833
Denis Bogatirov <denis.bohatyrov@computools.com>
2023-09-13 12:57:28 +0300

    Refactor types_utils, extract common methods

--- lib/lints/avoid_unnecessary_type_casts/avoid_unnecessary_type_casts_visitor.dart
+++ lib/lints/avoid_unnecessary_type_casts/avoid_unnecessary_type_casts_visitor.dart
@@ -23,8 +23,6 @@
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
-import 'package:analyzer/dart/element/nullability_suffix.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:solid_lints/utils/typecast_utils.dart';
 
 /// AST Visitor which finds all as expressions and checks if they are
@@ -41,71 +39,18 @@ class AvoidUnnecessaryTypeCastsVisitor extends RecursiveAstVisitor<void> {
 
     final objectType = node.expression.staticType;
     final castedType = node.type.type;
-    if (_isUselessTypeCheck(objectType, castedType)) {
+
+    if (objectType == null || castedType == null) {
+      return;
+    }
+
+    final typeCast = TypeCast(
+      source: objectType,
+      target: castedType,
+    );
+
+    if (typeCast.isUnnecessaryTypeCheck()) {
       _expressions[node] = 'as';
     }
-  }
-
-  bool _isUselessTypeCheck(DartType? objectType, DartType? castedType) {
-    if (objectType == null || castedType == null) {
-      return false;
-    }
-
-    if (_checkNullableCompatibility(objectType, castedType)) {
-      return false;
-    }
-
-    final objectCastedType =
-        _foundCastedTypeInObjectTypeHierarchy(objectType, castedType);
-    if (objectCastedType == null) {
-      return false;
-    }
-
-    if (!_checkGenerics(objectCastedType, castedType)) {
-      return false;
-    }
-
-    return true;
-  }
-
-  bool _checkNullableCompatibility(DartType objectType, DartType castedType) {
-    final isObjectTypeNullable =
-        objectType.nullabilitySuffix != NullabilitySuffix.none;
-    final isCastedTypeNullable =
-        castedType.nullabilitySuffix != NullabilitySuffix.none;
-
-    // Only one case `Type? is Type` always valid assertion case.
-    return isObjectTypeNullable && !isCastedTypeNullable;
-  }
-
-  DartType? _foundCastedTypeInObjectTypeHierarchy(
-    DartType objectType,
-    DartType castedType,
-  ) {
-    final typeCast = TypeCast(source: objectType, target: castedType);
-
-    return typeCast.castTypeInHierarchy();
-  }
-
-  bool _checkGenerics(DartType objectType, DartType castedType) {
-    if (objectType is! ParameterizedType || castedType is! ParameterizedType) {
-      return false;
-    }
-
-    final length = objectType.typeArguments.length;
-    if (length != castedType.typeArguments.length) {
-      return false;
-    }
-
-    for (var argumentIndex = 0; argumentIndex < length; argumentIndex++) {
-      if (!_isUselessTypeCheck(
-        objectType.typeArguments[argumentIndex],
-        castedType.typeArguments[argumentIndex],
-      )) {
-        return false;
-      }
-    }
-
-    return true;
   }
 }
