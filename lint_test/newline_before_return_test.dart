7179baa4ea260487362703da3882113a316f3366
DenisBogatirov <denis.bogatirov@gmail.com>
2023-09-15 12:09:44 +0300

    Feature/newline before return (#52)
    * Add newline-before-return rule
    
    * Fix typos, remove unnecessary negation
    
    * Add tests for newline-before-return
    
    ---------
    
    Co-authored-by: Denis Bogatirov <denis.bohatyrov@computools.com>

---
+++ lint_test/newline_before_return_test.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: unused_local_variable
+
+/// Check the `newline-before-return` rule
+class Foo {
+  int method() {
+    final a = 0;
+    // expect_lint: newline-before-return
+    return 1;
+  }
+
+  void anotherMethod() {
+    final a = 1;
+    // expect_lint: newline-before-return
+    return;
+  }
+
+  void bar(void Function()) {
+    return;
+  }
+}
+
+void fun() {
+  final foo = Foo();
+  foo.bar(() {
+    // This comment is ignored and line above is checked to be a newline
+    return;
+  });
+  foo.bar(() {
+    final a = 1;
+    // expect_lint: newline-before-return
+    return;
+  });
+  // expect_lint: newline-before-return
+  return;
+}
