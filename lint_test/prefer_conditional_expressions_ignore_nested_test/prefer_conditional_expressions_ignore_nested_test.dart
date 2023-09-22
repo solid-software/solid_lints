1f74e1bab0374133175582caf16b06ff5ac4f662
DenisBogatirov <denis.bogatirov@gmail.com>
2023-09-22 13:24:12 +0300

    Added prefer condtional expressions rule and fix (#59)
    * Add prefer-conditional-expressions rule and fix
    
    * Add tests for prefer-conditional-expressions rule
    
    * fix nested test plugin path
    
    * Fix tests after merge
    
    * Update lint_test/prefer_conditional_expressions_ignore_nested_test/prefer_conditional_expressions_ignore_nested_test.dart
    
    ---------
    
    Co-authored-by: Denis Bogatirov <denis.bohatyrov@computools.com>
    Co-authored-by: Yurii Prykhodko <144313329+yurii-prykhodko-solid@users.noreply.github.com>

---
+++ lint_test/prefer_conditional_expressions_ignore_nested_test/prefer_conditional_expressions_ignore_nested_test.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: unused_local_variable
+// ignore_for_file: cyclomatic_complexity
+// ignore_for_file: no-equal-then-else
+
+/// Check the `prefer-conditional-expressions` rule ignore-nested option
+void fun() {
+  int _result = 0;
+
+  // Allowed because ignore-nested flag is enabled
+  if (1 > 0) {
+    _result = 1 > 2 ? 2 : 1;
+  } else {
+    _result = 0;
+  }
+}
