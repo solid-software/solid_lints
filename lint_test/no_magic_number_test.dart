eb00fb4bb9edbd761b3d6fb6a6a20dead1a52e74
solid-vovabeloded <41615621+solid-vovabeloded@users.noreply.github.com>
2023-11-15 18:49:46 +0200

    Allow magic numbers for default values (#72)
    * Allow magic numbers for default values
    
    * More tests
    
    ---------
    
    Co-authored-by: vladimir-beloded <x-volodymyr.beloded@transcarent.com>

--- lint_test/no_magic_number_test.dart
+++ lint_test/no_magic_number_test.dart
@@ -1,4 +1,7 @@
-// ignore_for_file: unused_local_variable, prefer_match_file_name
+// ignore_for_file: unused_local_variable
+// ignore_for_file: prefer_match_file_name
+// ignore_for_file: avoid_unused_parameters
+// ignore_for_file: no_empty_block
 
 /// Check the `no_magic_number` rule
 
@@ -52,6 +55,27 @@ void fun() {
   // Allowed in indexed expression
   final result = list[1];
 
-  // Allowed in DateTime because it doesn't have cons constructor
+  // Allowed in DateTime because it doesn't have const constructor
   final apocalypse = DateTime(2012, 12, 21);
+}
+
+// Allowed for defaults in constructors and methods.
+class DefaultValues {
+  final int value;
+
+  DefaultValues.named({
+    this.value = 2,
+  });
+
+  DefaultValues.positional([
+    this.value = 3,
+  ]);
+
+  void methodWithNamedParam({int value = 4}) {}
+
+  void methodWithPositionalParam([int value = 5]) {}
+}
+
+void topLevelFunctionWithDefaultParam({int value = 6}) {
+  ({int value = 7}) {};
 }
