ee44662a34d99400814d1b736976b87ee148a047
vladimir-beloded <x-volodymyr.beloded@transcarent.com>
2023-11-15 17:34:52 +0200

    More tests

--- lint_test/no_magic_number_test.dart
+++ lint_test/no_magic_number_test.dart
@@ -55,7 +55,7 @@ void fun() {
   // Allowed in indexed expression
   final result = list[1];
 
-  // Allowed in DateTime because it doesn't have cons constructor
+  // Allowed in DateTime because it doesn't have const constructor
   final apocalypse = DateTime(2012, 12, 21);
 }
 
@@ -74,4 +74,8 @@ class DefaultValues {
   void methodWithNamedParam({int value = 4}) {}
 
   void methodWithPositionalParam([int value = 5]) {}
+}
+
+void topLevelFunctionWithDefaultParam({int value = 6}) {
+  ({int value = 7}) {};
 }
