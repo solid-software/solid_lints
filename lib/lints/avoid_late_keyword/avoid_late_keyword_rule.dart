6aae62c99213f3729d7e97ddeddba60f96fe7483
maxxlab <42914015+maxxlab@users.noreply.github.com>
2023-11-17 12:23:59 +0200

    Fix avoid-late if initialized (#71)

--- lib/lints/avoid_late_keyword/avoid_late_keyword_rule.dart
+++ lib/lints/avoid_late_keyword/avoid_late_keyword_rule.dart
@@ -1,10 +1,13 @@
+import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:solid_lints/lints/avoid_late_keyword/models/avoid_late_keyword_parameters.dart';
 import 'package:solid_lints/models/rule_config.dart';
 import 'package:solid_lints/models/solid_lint_rule.dart';
+import 'package:solid_lints/utils/types_utils.dart';
 
 /// A `late` keyword rule which forbids using it to avoid runtime exceptions.
-class AvoidLateKeywordRule extends SolidLintRule {
+class AvoidLateKeywordRule extends SolidLintRule<AvoidLateKeywordParameters> {
   /// The [LintCode] of this lint rule that represents
   /// the error whether we use `late` keyword.
   static const lintName = 'avoid_late_keyword';
@@ -17,6 +20,7 @@ class AvoidLateKeywordRule extends SolidLintRule {
     final rule = RuleConfig(
       configs: configs,
       name: lintName,
+      paramsParser: AvoidLateKeywordParameters.fromJson,
       problemMessage: (_) => 'Avoid using the "late" keyword. '
           'It may result in runtime exceptions.',
     );
@@ -31,9 +35,37 @@ class AvoidLateKeywordRule extends SolidLintRule {
     CustomLintContext context,
   ) {
     context.registry.addVariableDeclaration((node) {
-      if (node.declaredElement?.isLate ?? false) {
+      if (_shouldLint(node)) {
         reporter.reportErrorForNode(code, node);
       }
     });
+  }
+
+  bool _shouldLint(VariableDeclaration node) {
+    final isLateDeclaration = node.declaredElement?.isLate ?? false;
+    if (!isLateDeclaration) return false;
+
+    final hasIgnoredType = _hasIgnoredType(node);
+    if (hasIgnoredType) return false;
+
+    final allowInitialized = config.parameters.allowInitialized;
+    if (!allowInitialized) return true;
+
+    final hasInitializer = node.initializer != null;
+    return !hasInitializer;
+  }
+
+  bool _hasIgnoredType(VariableDeclaration node) {
+    final ignoredTypes = config.parameters.ignoredTypes.toSet();
+    if (ignoredTypes.isEmpty) return false;
+
+    final variableType = node.declaredElement?.type;
+    if (variableType == null) return false;
+
+    final checkedTypes = [variableType, ...variableType.supertypes]
+        .map((t) => t.getDisplayString(withNullability: false))
+        .toSet();
+
+    return checkedTypes.intersection(ignoredTypes).isNotEmpty;
   }
 }
--- lint_test/avoid_late_keyword_test.dart
+++ lint_test/avoid_late_keyword_test.dart
@@ -1,21 +1,64 @@
 // ignore_for_file: prefer_const_declarations, unused_local_variable, prefer_match_file_name
 // ignore_for_file: avoid_global_state
 
+class ColorTween {}
+
+class AnimationController {}
+
+class SubAnimationController extends AnimationController {}
+
+class NotAllowed {}
+
 /// Check "late" keyword fail
 ///
 /// `avoid_late_keyword`
+/// allow_initialized option enabled
 class AvoidLateKeyword {
-  /// expect_lint: avoid_late_keyword
+  late final ColorTween colorTween;
+
+  late final AnimationController controller1;
+
+  late final SubAnimationController controller2;
+
+  late final controller3 = AnimationController();
+
+  late final controller4 = SubAnimationController();
+
   late final field1 = 'string';
 
   /// expect_lint: avoid_late_keyword
-  late String field2;
+  late final String field2;
+
+  /// expect_lint: avoid_late_keyword
+  late final field3;
+
+  /// expect_lint: avoid_late_keyword
+  late final NotAllowed na1;
+
+  late final na2 = NotAllowed();
 
   void test() {
-    /// expect_lint: avoid_late_keyword
-    late final field3 = 'string';
+    late final ColorTween colorTween;
+
+    late final AnimationController controller1;
+
+    late final SubAnimationController controller2;
+
+    late final controller3 = AnimationController();
+
+    late final controller4 = SubAnimationController();
+
+    late final local1 = 'string';
 
     /// expect_lint: avoid_late_keyword
-    late String field4;
+    late final String local2;
+
+    /// expect_lint: avoid_late_keyword
+    late final local3;
+
+    /// expect_lint: avoid_late_keyword
+    late final NotAllowed na1;
+
+    late final na2 = NotAllowed();
   }
 }
