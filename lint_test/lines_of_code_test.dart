3da20e6c5e8c311037ae35d7c7efcbea2aa48473
Yurii Prykhodko <144313329+yurii-prykhodko-solid@users.noreply.github.com>
2023-11-23 00:59:45 +0200

    Fix function_lines_of_code (#85)
    * Fix function_lines_of_code
    
    * Format
    
    ---------
    
    Co-authored-by: Yurii Prykhodko <yurii.prykhodko@solid.software>

--- lint_test/lines_of_code_test.dart
+++ lint_test/lines_of_code_test.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: no_magic_number
+// ignore_for_file: no_magic_number, prefer_match_file_name
 
 import 'package:test/test.dart';
 
@@ -58,4 +58,133 @@ void linesOfCode() {
   test("addition", () {
     expect(1 + 1, equals(2));
   });
+  test("addition", () {
+    expect(1 + 1, equals(2));
+  });
+}
+
+class A {
+  /// expect_lint: function_lines_of_code
+  void b() {
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+  }
+}
+
+class B {
+  /// expect_lint: function_lines_of_code
+  void anon() {
+    test("addition", () {
+      expect(1 + 1, equals(2));
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+      test("addition", () {
+        expect(1 + 1, equals(2));
+      });
+    });
+  }
+
+  void m() {
+    test("addition", () {
+      expect(1 + 1, equals(2));
+    });
+  }
 }
