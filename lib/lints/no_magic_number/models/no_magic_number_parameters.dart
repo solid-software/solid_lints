fe24f83ba6c3909f27cf3942a22387e5c543014f
Yurii Prykhodko <yurii.prykhodko+github.us@solid.software>
2024-01-17 22:08:11 +0200

    Update no_magic_number doc

--- lib/lints/no_magic_number/models/no_magic_number_parameters.dart
+++ lib/lints/no_magic_number/models/no_magic_number_parameters.dart
@@ -5,11 +5,40 @@ class NoMagicNumberParameters {
   static const _allowedInWidgetParamsConfigName = 'allowed_in_widget_params';
   static const _defaultMagicNumbers = [-1, 0, 1];
 
-  /// List of allowed numbers
+  /// List of numbers excluded from analysis
+  ///
+  /// Defaults to numbers commonly used for increments and index access:
+  /// `[-1, 0, 1]`.
   final Iterable<num> allowedNumbers;
 
-  /// The flag indicates whether magic numbers are allowed as a Widget instance
-  /// parameter.
+  /// Boolean flag, toggles analysis for raw numbers within Widget constructor
+  /// call parameters.
+  ///
+  /// When flag is set to `false`, it warns about any non-const numbers in
+  /// your layout:
+  ///
+  /// ```dart
+  /// Widget build() {
+  ///   return MyWidget(
+  ///     decoration: MyWidgetDecoration(size: 12), // LINT
+  ///     value: 23,                                // LINT
+  ///     child: const SizedBox(width: 20)          // allowed for const
+  ///   );
+  /// }
+  /// ```
+  ///
+  /// When flag is set to `true`, it allows non-const layouts with raw numbers:
+  ///
+  /// ```dart
+  /// Widget build() {
+  ///   return MyWidget(
+  ///     decoration: MyWidgetDecoration(size: 12), // OK
+  ///     value: 23, // OK
+  ///     child: const SizedBox(width: 20) // OK as it was
+  ///   );
+  /// }
+  /// ```
+  ///
   final bool allowedInWidgetParams;
 
   /// Constructor for [NoMagicNumberParameters] model
