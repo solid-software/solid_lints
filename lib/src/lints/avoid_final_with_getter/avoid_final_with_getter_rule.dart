f49be9d398d2b880008bde36cb28f8444cf69ba1
alexiuk.genius <alexiuk.genius@gmail.com>
2024-04-17 16:56:20 +0200

    add avoid_final_with_getter rule

---
+++ lib/src/lints/avoid_final_with_getter/avoid_final_with_getter_rule.dart
@@ -0,0 +1,67 @@
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:solid_lints/src/lints/avoid_final_with_getter/avoid_final_with_getter_visitor.dart';
+import 'package:solid_lints/src/models/rule_config.dart';
+import 'package:solid_lints/src/models/solid_lint_rule.dart';
+
+/// Avoid using final private fields with getters.
+///
+/// Final private variables used in a pair with a getter
+/// must be changed to a final public type without a getter
+/// because it is the same as a public field.
+///
+/// ### Example
+///
+/// #### BAD:
+///
+/// ```dart
+/// class MyClass {
+///   final int _myField = 0;
+///
+///   int get myField => _myField;
+/// }
+/// ```
+///
+/// #### GOOD:
+///
+/// ```dart
+/// class MyClass {
+///   final int myField = 0;
+/// }
+/// ```
+///
+class AvoidFinalWithGetterRule extends SolidLintRule {
+  /// The [LintCode] of this lint rule that represents
+  /// the error whether we use final private fields with getters.
+  static const lintName = 'avoid_final_with_getter';
+
+  AvoidFinalWithGetterRule._(super.config);
+
+  /// Creates a new instance of [AvoidFinalWithGetterRule]
+  /// based on the lint configuration.
+  factory AvoidFinalWithGetterRule.createRule(CustomLintConfigs configs) {
+    final rule = RuleConfig(
+      configs: configs,
+      name: lintName,
+      problemMessage: (_) => 'Avoid final private fields with getters.',
+    );
+
+    return AvoidFinalWithGetterRule._(rule);
+  }
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addCompilationUnit((node) {
+      final visitor = AvoidFinalWithGetterVisitor();
+      node.accept(visitor);
+
+      for (final variable in visitor.variables) {
+        reporter.reportErrorForNode(code, variable);
+      }
+    });
+  }
+}
---
+++ lint_test/avoid_final_with_getter_test.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: type_annotate_public_apis, prefer_match_file_name, unused_local_variable
+
+/// Check final private field with getter fail
+/// `avoid_final_with_getter`
+
+class Fail {
+  // expect_lint: avoid_final_with_getter
+  final int _myField = 0;
+
+  int get myField => _myField;
+
+  int get myField2 => _myField + 1;
+}
+
+class Good {
+  final int myField = 0;
+}
