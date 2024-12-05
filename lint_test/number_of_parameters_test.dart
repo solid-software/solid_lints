baa77f12640901c0162f2048207ed4a64b4cac21
shaark <shaark@servitor.solutions>
2024-12-05 10:57:35 +0200

    issue-124. exclude copyWith from nubmer of parameters

--- lint_test/number_of_parameters_test.dart
+++ lint_test/number_of_parameters_test.dart
@@ -18,3 +18,39 @@ class Exclude {
     return a + b + c;
   }
 }
+
+class UserDto {
+  final String email;
+  final String firstName;
+  final String id;
+  final String imageUrl;
+  final String lastName;
+
+  final String phone;
+  const UserDto({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.imageUrl,
+    required this.email,
+    required this.phone,
+  });
+
+  UserDto copyWith({
+    String? email,
+    String? firstName,
+    String? id,
+    String? imageUrl,
+    String? lastName,
+    String? phone,
+  }) {
+    return UserDto(
+      email: email ?? this.email,
+      firstName: firstName ?? this.firstName,
+      id: id ?? this.id,
+      imageUrl: imageUrl ?? this.imageUrl,
+      lastName: lastName ?? this.lastName,
+      phone: phone ?? this.phone,
+    );
+  }
+}
