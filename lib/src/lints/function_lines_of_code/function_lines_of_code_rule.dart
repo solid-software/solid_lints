1e85fa532f3a7f4d969eb55395bf570d90aca141
elijah mac <aygistov.ilya@gmail.com>
2025-01-30 15:03:46 +0200

    Fixed function_lines_of_code_rule.dart

--- lib/src/lints/function_lines_of_code/function_lines_of_code_rule.dart
+++ lib/src/lints/function_lines_of_code/function_lines_of_code_rule.dart
@@ -50,15 +50,25 @@ class FunctionLinesOfCodeRule
   ) {
     void checkNode(AstNode node) => _checkNode(resolver, reporter, node);
 
-    context.registry.addDeclaration((declarationNode) {
-      final isIgnored = config.parameters.exclude.shouldIgnore(declarationNode);
+    void checkDeclarationNode(Declaration node) {
+      final isIgnored = config.parameters.exclude.shouldIgnore(node);
+      if (isIgnored) {
+        return;
+      }
+      checkNode(node);
+    }
 
-      if (isIgnored) return;
+    // Check for an anonymous function
+    void checkFunctionExpressionNode(FunctionExpression node) {
+      if (node.parent is FunctionDeclaration) {
+        return;
+      }
+      checkNode(node);
+    }
 
-      context.registry.addMethodDeclaration(checkNode);
-      context.registry.addFunctionDeclaration(checkNode);
-      context.registry.addFunctionExpression(checkNode);
-    });
+    context.registry.addFunctionDeclaration(checkDeclarationNode);
+    context.registry.addMethodDeclaration(checkDeclarationNode);
+    context.registry.addFunctionExpression(checkFunctionExpressionNode);
   }
 
   void _checkNode(
