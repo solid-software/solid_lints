fdd18e3a3a2bc9e47f43436da927abbeb98527b9
iimironchuk <iramyronchuk26@gmail.com>
2025-04-30 17:54:28 +0300

    added exclude_entity to analysis_options.yaml

--- lint_test/prefer_match_file_name_extension_test.dart
+++ lint_test/prefer_match_file_name_extension_test.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_element, unused_field
 
 /// Check the `prefer_match_file_name` rule
+/// To trigger lint delete "extension" from exclude_entity in analysis_options.yaml
 // expect_lint: prefer_match_file_name
 extension WrongExtension on List {}
