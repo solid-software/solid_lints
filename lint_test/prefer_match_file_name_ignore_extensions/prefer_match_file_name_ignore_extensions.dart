90a987aec6b82889bd7b1ef510ce8ee3ce088988
iimironchuk <iramyronchuk26@gmail.com>
2025-04-30 19:53:15 +0300

    separate test case file + several declarations added

---
+++ lint_test/prefer_match_file_name_ignore_extensions/prefer_match_file_name_ignore_extensions.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unused_element, unused_field
+
+/// `prefer_match_file_name` rule will be ignored by this extension because of
+/// exclude_entity:
+/// - extension
+/// in analysis_options.yaml
+extension DefaultExtension on String {}
+
+// expect_lint: prefer_match_file_name
+abstract class WrongNamedClass {}
+
+/// Only first public element declaration is checked
+class PreferMatchFileNameIgnoreExtensions {}
