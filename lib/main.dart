65929c19665ef2ea14a5311863737a9b6e3fabbd
Islam-Shaaban-Ibrahim <eslamshaaban01120@gmail.com>
2026-04-15 05:47:03 +0200

    applied code review suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:analysis_server_plugin/plugin.dart';
 import 'package:analysis_server_plugin/registry.dart';
-import 'package:solid_lints/src/lints/avoid_debug_print_in_release/avoid_debug_print_in_release_rule.dart';
 import 'package:solid_lints/src/lints/avoid_global_state/avoid_global_state_rule.dart';
 
+/// create plugin
 final plugin = SolidLintsPlugin();
 
+/// create plugin class
 class SolidLintsPlugin extends Plugin {
   @override
   String get name => 'solid_lints';
--- lib/src/lints/avoid_global_state/avoid_global_state_rule.dart
+++ lib/src/lints/avoid_global_state/avoid_global_state_rule.dart
@@ -1,9 +1,8 @@
 import 'package:analyzer/analysis_rule/analysis_rule.dart';
 import 'package:analyzer/analysis_rule/rule_context.dart';
 import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
-import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/error/error.dart';
+import 'package:solid_lints/src/lints/avoid_global_state/avoid_global_state_rule_visitor.dart';
 
 /// Avoid top-level and static mutable variables.
 ///
@@ -63,56 +62,9 @@ class AvoidGlobalStateRule extends AnalysisRule {
     RuleVisitorRegistry registry,
     RuleContext context,
   ) {
-    final visitor = _Visitor(this);
+    final visitor = AvoidGlobalStateRuleVisitor(this);
 
     registry.addTopLevelVariableDeclaration(this, visitor);
     registry.addFieldDeclaration(this, visitor);
-  }
-}
-
-class _Visitor extends SimpleAstVisitor<void> {
-  final AvoidGlobalStateRule rule;
-
-  _Visitor(this.rule);
-
-  @override
-  void visitTopLevelVariableDeclaration(TopLevelVariableDeclaration node) {
-    for (final variable in node.variables.variables) {
-      if (_isPublicMutable(variable)) {
-        rule.reportAtNode(variable);
-      }
-    }
-  }
-
-  @override
-  void visitFieldDeclaration(FieldDeclaration node) {
-    if (!node.isStatic) return;
-
-    for (final variable in node.fields.variables) {
-      if (_isPublicMutable(variable)) {
-        rule.reportAtNode(variable);
-      }
-    }
-  }
-
-  /// Returns true if the variable is mutable and not private.
-  bool _isPublicMutable(VariableDeclaration variable) {
-    return _isMutable(variable) && !_isPrivate(variable);
-  }
-
-  /// A variable is mutable if it is not final or const.
-  bool _isMutable(VariableDeclaration variable) {
-    final element = variable.declaredFragment?.element;
-
-    final isFinal = element?.isFinal ?? false;
-    final isConst = element?.isConst ?? false;
-
-    return !isFinal && !isConst;
-  }
-
-  /// A variable is private if its element is private.
-  bool _isPrivate(VariableDeclaration variable) {
-    final element = variable.declaredFragment?.element;
-    return element?.isPrivate ?? false;
   }
 }
