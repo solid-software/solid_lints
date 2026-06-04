e6c78e64872a704675175af32b25e5dc7f2d7a2a
Andrew Bekhiet <andrew.bekhiet@solid.software>
2026-06-04 22:53:14 +0300

    fix: also rename private field references

--- lib/src/lints/avoid_final_with_getter/fixes/avoid_final_with_getter_fix.dart
+++ lib/src/lints/avoid_final_with_getter/fixes/avoid_final_with_getter_fix.dart
@@ -2,11 +2,11 @@ import 'package:analysis_server_plugin/edit/dart/correction_producer.dart';
 import 'package:analysis_server_plugin/edit/dart/dart_fix_kind_priority.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/source/source_range.dart';
 import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
 import 'package:analyzer_plugin/utilities/fixes/fixes.dart';
 import 'package:solid_lints/src/lints/avoid_final_with_getter/avoid_final_with_getter_rule.dart';
 import 'package:solid_lints/src/lints/avoid_final_with_getter/visitors/getter_variable_visitor.dart';
+import 'package:solid_lints/src/lints/avoid_final_with_getter/visitors/variable_references_visitor.dart';
 
 /// A Quick fix for [AvoidFinalWithGetterRule] rule
 class AvoidFinalWithGetterFix extends ResolvedCorrectionProducer {
@@ -42,18 +42,40 @@ class AvoidFinalWithGetterFix extends ResolvedCorrectionProducer {
       final compilationUnit = node.thisOrAncestorOfType<CompilationUnit>();
       if (compilationUnit == null) return;
 
-      final visitor = GetterVariableVisitor(getterNode);
-      compilationUnit.accept(visitor);
+      final getterVariableVisitor = GetterVariableVisitor(getterNode);
+      compilationUnit.accept(getterVariableVisitor);
 
-      final variableDeclaration = visitor.variable;
+      final variableDeclaration = getterVariableVisitor.variable;
       if (variableDeclaration == null) return;
 
-      final variablePrivatePrefixRange =
-          SourceRange(variableDeclaration.name.offset, 1);
+      final referencesVisitor = VariableReferencesVisitor(variableDeclaration);
+      compilationUnit.accept(referencesVisitor);
+
+      final variableReferences = referencesVisitor.references;
+
+      final variableName = variableDeclaration.name.lexeme;
+      final newPublicVariableName = variableName.startsWith('_')
+          ? variableName.substring(1)
+          : variableName;
 
       await builder.addDartFileEdit(file, (builder) {
         builder.addDeletion(getterNode.sourceRange);
-        builder.addDeletion(variablePrivatePrefixRange);
+
+        builder.addSimpleReplacement(
+          variableDeclaration.name.sourceRange,
+          newPublicVariableName,
+        );
+
+        for (final reference in variableReferences) {
+          if (reference.sourceRange.intersects(getterNode.sourceRange)) {
+            continue;
+          }
+
+          builder.addSimpleReplacement(
+            reference.sourceRange,
+            newPublicVariableName,
+          );
+        }
 
         builder.format(compilationUnit.sourceRange);
       });
