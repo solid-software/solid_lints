9b647514abc9a5b620a7a95654df9c0e12c64c1a
Andrew Bekhiet <andrew.bekhiet@solid.software>
2026-06-10 12:47:14 +0300

    fix: use fix kind instead of assist kind

--- lib/src/lints/avoid_unnecessary_type_assertions/fixes/avoid_unnecessary_type_assertions_fix.dart
+++ lib/src/lints/avoid_unnecessary_type_assertions/fixes/avoid_unnecessary_type_assertions_fix.dart
@@ -2,14 +2,14 @@ import 'package:analysis_server_plugin/edit/dart/correction_producer.dart';
 import 'package:analysis_server_plugin/edit/dart/dart_fix_kind_priority.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/source/source_range.dart';
-import 'package:analyzer_plugin/utilities/assist/assist.dart';
 import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
+import 'package:analyzer_plugin/utilities/fixes/fixes.dart';
 import 'package:solid_lints/src/lints/avoid_unnecessary_type_assertions/avoid_unnecessary_type_assertions_rule.dart';
 
 /// A Quick fix for `avoid_unnecessary_type_assertions` rule
 /// Suggests to remove unnecessary assertions
 class AvoidUnnecessaryTypeAssertionsFix extends ResolvedCorrectionProducer {
-  static const _avoidUnnecessaryTypeAssertionsAssistKind = AssistKind(
+  static const _avoidUnnecessaryTypeAssertionsFixKind = FixKind(
     'solid_lints.fix.${AvoidUnnecessaryTypeAssertionsRule.lintName}',
     DartFixKindPriority.standard,
     'Remove the unnecessary "{0}"',
@@ -21,7 +21,7 @@ class AvoidUnnecessaryTypeAssertionsFix extends ResolvedCorrectionProducer {
   List<String>? get fixArguments => [_itemToDelete];
 
   @override
-  AssistKind get assistKind => _avoidUnnecessaryTypeAssertionsAssistKind;
+  FixKind get fixKind => _avoidUnnecessaryTypeAssertionsFixKind;
 
   @override
   CorrectionApplicability get applicability =>
