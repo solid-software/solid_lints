13fdf86d5690e0ba929bfa9b3b9aee7c1ec1b697
Andrew Bekhiet <andrew.bekhiet@solid.software>
2026-06-18 11:55:27 +0300

    refactor: migrate avoid_unnecessary_type_assertions (#282)
    * refactor: migrate unnecessary_type_assertion
    
    * refactor: migrate unnecessary_type_assertion fix
    
    * fix: use fix kind instead of assist kind
    
    * fix: use thisOrAncestorOfType to get the nearest node of type T
    fix: remove leading space when removing is operator
    
    * feat: display removed part in the fix message
    refactor: simplify removal code
    
    * fix: handle custom iterable types
    
    * test: add test for custom iterables with subtypes
    
    * refactor: improve readability
    
    * fix: improve AutoTestLintOffsets reliability
    handles edge cases when there are multiple similar code fragments

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'package:solid_lints/src/lints/avoid_global_state/avoid_global_state_rule
 import 'package:solid_lints/src/lints/avoid_non_null_assertion/avoid_non_null_assertion_rule.dart';
 import 'package:solid_lints/src/lints/avoid_returning_widgets/avoid_returning_widgets_rule.dart';
 import 'package:solid_lints/src/lints/avoid_returning_widgets/models/avoid_returning_widgets_parameters.dart';
+import 'package:solid_lints/src/lints/avoid_unnecessary_type_assertions/avoid_unnecessary_type_assertions_rule.dart';
+import 'package:solid_lints/src/lints/avoid_unnecessary_type_assertions/fixes/avoid_unnecessary_type_assertions_fix.dart';
 import 'package:solid_lints/src/lints/double_literal_format/double_literal_format_rule.dart';
 import 'package:solid_lints/src/lints/double_literal_format/fixes/double_literal_format_fix.dart';
 import 'package:solid_lints/src/lints/proper_super_calls/proper_super_calls_rule.dart';
@@ -30,11 +32,14 @@ class SolidLintsPlugin extends Plugin {
   void register(PluginRegistry registry) {
     final analysisLoader = AnalysisOptionsLoader();
 
+    final avoidUnnecessaryTypeAssertionsRule =
+        AvoidUnnecessaryTypeAssertionsRule();
     final doubleLiteralFormatRule = DoubleLiteralFormatRule();
     final lintRules = [
       AvoidFinalWithGetterRule(),
       AvoidGlobalStateRule(),
       AvoidNonNullAssertionRule(),
+      avoidUnnecessaryTypeAssertionsRule,
       AvoidDebugPrintInReleaseRule(),
       doubleLiteralFormatRule,
       ProperSuperCallsRule(),
@@ -55,6 +60,10 @@ class SolidLintsPlugin extends Plugin {
     registry.registerFixForRule(
       AvoidFinalWithGetterRule.code,
       AvoidFinalWithGetterFix.new,
+    );
+    registry.registerFixForRule(
+      avoidUnnecessaryTypeAssertionsRule.diagnosticCode,
+      AvoidUnnecessaryTypeAssertionsFix.new,
     );
   }
 }
