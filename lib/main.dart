ad8eba2af5adff67ee333df3fd053c05efd53386
Andrew Bekhiet <andrew.bekhiet@solid.software>
2026-06-19 12:46:31 +0300

    remove avoid_unnecessary_type_casts use dart linter's `unnecessary_cast` instead

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,6 @@ import 'package:solid_lints/src/lints/avoid_non_null_assertion/avoid_non_null_as
 import 'package:solid_lints/src/lints/avoid_returning_widgets/avoid_returning_widgets_rule.dart';
 import 'package:solid_lints/src/lints/avoid_unnecessary_type_assertions/avoid_unnecessary_type_assertions_rule.dart';
 import 'package:solid_lints/src/lints/avoid_unnecessary_type_assertions/fixes/avoid_unnecessary_type_assertions_fix.dart';
-import 'package:solid_lints/src/lints/avoid_unnecessary_type_casts/avoid_unnecessary_type_casts_rule.dart';
-import 'package:solid_lints/src/lints/avoid_unnecessary_type_casts/fixes/avoid_unnecessary_type_casts_fix.dart';
 import 'package:solid_lints/src/lints/avoid_unused_parameters/avoid_unused_parameters_rule.dart';
 import 'package:solid_lints/src/lints/double_literal_format/double_literal_format_rule.dart';
 import 'package:solid_lints/src/lints/double_literal_format/fixes/double_literal_format_fix.dart';
@@ -37,11 +35,9 @@ class SolidLintsPlugin extends Plugin {
     final avoidUnnecessaryTypeAssertionsRule =
         AvoidUnnecessaryTypeAssertionsRule();
     final doubleLiteralFormatRule = DoubleLiteralFormatRule();
-    final avoidUnnecessaryTypeCastsRule = AvoidUnnecessaryTypeCastsRule();
 
     final lintRules = [
       AvoidFinalWithGetterRule(),
-      avoidUnnecessaryTypeCastsRule,
       AvoidGlobalStateRule(),
       AvoidNonNullAssertionRule(),
       avoidUnnecessaryTypeAssertionsRule,
@@ -71,10 +67,6 @@ class SolidLintsPlugin extends Plugin {
     registry.registerFixForRule(
       avoidUnnecessaryTypeAssertionsRule.diagnosticCode,
       AvoidUnnecessaryTypeAssertionsFix.new,
-    );
-    registry.registerFixForRule(
-      avoidUnnecessaryTypeCastsRule.diagnosticCode,
-      AvoidUnnecessaryTypeCastsFix.new,
     );
   }
 }
