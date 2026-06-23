8edd5219ef0a76c19481a56dd907cbfbe5d862cb
Andrew Bekhiet <andrew.bekhiet@solid.software>
2026-06-23 15:19:50 +0300

    refactor: use AutoTestLintOffsets

--- test/lints/prefer_first/prefer_first_rule_test.dart
+++ test/lints/prefer_first/prefer_first_rule_test.dart
@@ -2,6 +2,8 @@ import 'package:analyzer_testing/analysis_rule/analysis_rule.dart';
 import 'package:solid_lints/src/lints/prefer_first/prefer_first_rule.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
+import '../auto_test_lint_offsets.dart';
+
 void main() {
   defineReflectiveSuite(() {
     defineReflectiveTests(PreferFirstRuleTest);
@@ -9,7 +11,7 @@ void main() {
 }
 
 @reflectiveTest
-class PreferFirstRuleTest extends AnalysisRuleTest {
+class PreferFirstRuleTest extends AnalysisRuleTest with AutoTestLintOffsets {
   @override
   void setUp() {
     rule = PreferFirstRule();
@@ -17,19 +19,16 @@ class PreferFirstRuleTest extends AnalysisRuleTest {
   }
 
   void test_reports_on_list_index_access_with_zero_literal() async {
-    await assertDiagnostics(
-      r'''
+    await assertAutoDiagnostics('''
 final list1 = [0, 1, 2, 3];
-var a = list1[0];
+var a = ${expectLint('list1[0]')};
 
 void main () {
   final list2 = [1, 0, 2, 3];
-  list1[0];
-  list2[0];
+  ${expectLint('list1[0]')};
+  ${expectLint('list2[0]')};
 }
-''',
-      [lint(36, 8), lint(94, 8), lint(106, 8)],
-    );
+''');
   }
 
   void
@@ -47,32 +46,26 @@ var c = list[zeroVar];
   }
 
   void test_reports_on_list_subclasses() async {
-    await assertDiagnostics(
-      r'''
+    await assertAutoDiagnostics('''
 abstract class MyList<T> implements List<T> {}
 
 T getFirst<T>(MyList<T> list) {
-  return list[0];
+  return ${expectLint('list[0]')};
 }
-''',
-      [lint(89, 7)],
-    );
+''');
   }
 
   void test_reports_on_element_at_access_with_zero_literal() async {
-    await assertDiagnostics(
-      r'''
+    await assertAutoDiagnostics('''
 final list1 = [0, 1, 2, 3];
-var a = list1.elementAt(0);
+var a = ${expectLint('list1.elementAt(0)')};
 
 void main () {
   final list2 = [1, 0, 2, 3];
-  list1.elementAt(0);
-  list2.elementAt(0);
+  ${expectLint('list1.elementAt(0)')};
+  ${expectLint('list2.elementAt(0)')};
 }
-''',
-      [lint(36, 18), lint(104, 18), lint(126, 18)],
-    );
+''');
   }
 
   void
@@ -90,24 +83,21 @@ var c = list.elementAt(zeroVar);
   }
 
   void test_reports_on_iterable_subclasses() async {
-    await assertDiagnostics(
-      r'''
+    await assertAutoDiagnostics('''
 abstract class MyIterable<T> implements Iterable<T> {}
 
 T getFirst<T>(MyIterable<T> iterable) {
-  return iterable.elementAt(0);
+  return ${expectLint('iterable.elementAt(0)')};
 }
 
 void main () {
   final set = {0, 1, 2, 3};
   final map = {0: 0, 1: 1, 2: 2, 3: 3};
 
-  set.elementAt(0);
-  map.keys.elementAt(0);
-  map.values.elementAt(0);
+  ${expectLint('set.elementAt(0)')};
+  ${expectLint('map.keys.elementAt(0)')};
+  ${expectLint('map.values.elementAt(0)')};
 }
-''',
-      [lint(105, 21), lint(217, 16), lint(237, 21), lint(262, 23)],
-    );
+''');
   }
 }
