9de46796908247e2dbad01d69e3a36b3a4c24dc2
Illia Aihistov <illia.aihistov-us@solid.software>
2026-06-25 12:08:35 +0300

    refactor: migrate number_of_parameters

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:solid_lints/src/lints/cyclomatic_complexity/cyclomatic_complexit
 import 'package:solid_lints/src/lints/double_literal_format/double_literal_format_rule.dart';
 import 'package:solid_lints/src/lints/double_literal_format/fixes/double_literal_format_fix.dart';
 import 'package:solid_lints/src/lints/function_lines_of_code/function_lines_of_code_rule.dart';
+import 'package:solid_lints/src/lints/number_of_parameters/number_of_parameters_rule.dart';
 import 'package:solid_lints/src/lints/prefer_first/fixes/prefer_first_fix.dart';
 import 'package:solid_lints/src/lints/prefer_first/prefer_first_rule.dart';
 import 'package:solid_lints/src/lints/proper_super_calls/proper_super_calls_rule.dart';
@@ -61,6 +62,9 @@ class SolidLintsPlugin extends Plugin {
         analysisOptionsLoader: analysisLoader,
       ),
       CyclomaticComplexityRule(
+        analysisOptionsLoader: analysisLoader,
+      ),
+      NumberOfParametersRule(
         analysisOptionsLoader: analysisLoader,
       ),
       UseNearestContextRule(),
