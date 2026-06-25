f32d2ae97969fcffdfe42357b56428204cef4d13
Illia Aihistov <illia.aihistov@solid.software>
2026-06-25 16:50:21 +0300

    refactor: migrate no_empty_block (#299)
    * refactor: migrate no_empty_block
    
    * fix: allow multiple preceding comments to satisfy the todo check in no_empty_block rule
    
    * docs: add configuration examples to no_empty_block rule documentation
    
    * test: rename excluded method in no_empty_block_rule_test
    
    ---------
    
    Co-authored-by: Illia Aihistov <illia.aihistov-us@solid.software>

--- lib/src/lints/no_empty_block/visitors/no_empty_block_visitor.dart
+++ lib/src/lints/no_empty_block/visitors/no_empty_block_visitor.dart
@@ -21,26 +21,29 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import 'package:analyzer/analysis_rule/analysis_rule.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:solid_lints/src/common/parameters/excluded_identifiers_list_parameter.dart';
 
 const _todoComment = 'TODO';
 
 /// The AST visitor that will find all empty blocks, excluding catch blocks
 /// and blocks containing [_todoComment]
 class NoEmptyBlockVisitor extends RecursiveAstVisitor<void> {
+  final AnalysisRule _rule;
   final bool _allowWithComments;
-
-  final _emptyBlocks = <Block>[];
+  final ExcludedIdentifiersListParameter _exclude;
 
   /// Constructor for [NoEmptyBlockVisitor]
-  /// [_allowWithComments] indicates whether to allow empty blocks that contain
-  /// any comments
-  NoEmptyBlockVisitor({required bool allowWithComments})
-      : _allowWithComments = allowWithComments;
-
-  /// All empty blocks
-  Iterable<Block> get emptyBlocks => _emptyBlocks;
+  NoEmptyBlockVisitor({
+    required AnalysisRule rule,
+    required bool allowWithComments,
+    required ExcludedIdentifiersListParameter exclude,
+  }) : _rule = rule,
+       _allowWithComments = allowWithComments,
+       _exclude = exclude;
 
   @override
   void visitBlock(Block node) {
@@ -51,11 +54,25 @@ class NoEmptyBlockVisitor extends RecursiveAstVisitor<void> {
     if (_allowWithComments && _isPrecedingCommentAny(node)) return;
     if (_isPrecedingCommentToDo(node)) return;
 
-    _emptyBlocks.add(node);
+    final enclosingDeclaration = node.thisOrAncestorOfType<Declaration>();
+    if (enclosingDeclaration != null &&
+        _exclude.shouldIgnore(enclosingDeclaration)) {
+      return;
+    }
+
+    _rule.reportAtNode(node);
   }
 
-  static bool _isPrecedingCommentToDo(Block node) =>
-      node.endToken.precedingComments?.lexeme.contains(_todoComment) ?? false;
+  static bool _isPrecedingCommentToDo(Block node) {
+    Token? comment = node.endToken.precedingComments;
+    while (comment != null) {
+      if (comment.lexeme.contains(_todoComment)) {
+        return true;
+      }
+      comment = comment.next;
+    }
+    return false;
+  }
 
   static bool _isPrecedingCommentAny(Block node) =>
       node.endToken.precedingComments != null;
