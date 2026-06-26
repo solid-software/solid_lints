09da30ef0211f015ade690e76645dc1a7980c910
Illia Aihistov <illia.aihistov-us@solid.software>
2026-06-26 13:58:23 +0300

    style: fix doc comment line length in correction_utils.dart

--- lib/src/utils/correction_utils.dart
+++ lib/src/utils/correction_utils.dart
@@ -1,7 +1,8 @@
 import 'package:analysis_server_plugin/edit/correction_utils.dart';
 import 'package:analyzer/source/source_range.dart';
 
-/// Extension on [CorrectionUtils] to provide utilities for creating [SourceRange]s.
+/// Extension on [CorrectionUtils] to provide utilities for creating
+/// [SourceRange]s.
 extension CorrectionUtilsExtension on CorrectionUtils {
   /// Creates a [SourceRange] from [start] and [end] offsets.
   SourceRange createRange(int start, int end) =>
