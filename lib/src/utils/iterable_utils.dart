f9b09c1b3f6569d56b54be57c841fc9e92924fb0
Illia Aihistov <illia.aihistov-us@solid.software>
2026-06-26 14:31:07 +0300

    refactor: simplify pairwise implementation to use indexed element access

--- lib/src/utils/iterable_utils.dart
+++ lib/src/utils/iterable_utils.dart
@@ -4,13 +4,8 @@ extension IterablePairwise<T> on Iterable<T> {
   /// Returns an iterable of overlapping pairs of elements.
   /// For example, `[1, 2, 3].pairwise()` returns `[(1, 2), (2, 3)]`.
   Iterable<(T, T)> pairwise() sync* {
-    final iterator = this.iterator;
-    if (!iterator.moveNext()) return;
-
-    var previous = iterator.current;
-    while (iterator.moveNext()) {
-      yield (previous, iterator.current);
-      previous = iterator.current;
+    for (var i = 0; i + 1 < length; i++) {
+      yield (elementAt(i), elementAt(i + 1));
     }
   }
 }
